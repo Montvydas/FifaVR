00098bd6485bce5926c4b6cf4ab9754066c0c87b
Montvydas Klumbys <montvydas.klumbys@gmail.com>
2018-03-25 23:11:58 +0100

    Initial commit with code

---
+++ Assets/Scripts/RobotController.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using UnityEngine;
+
+public class RobotController : MonoBehaviour
+{
+    public float speed;
+//    private Rigidbody robot;
+    public Animator animator;
+
+    // Use this for initialization
+    void Start () 
+    {
+//        robot = GetComponent<Rigidbody> ();
+        animator = GetComponent<Animator>();
+    }
+	
+    // Update is called once per frame
+    void Update () 
+    {
+		
+    }
+
+    // All physics go there
+    void FixedUpdate() 
+    {
+        float moveHorizontal = Input.GetAxis ("Horizontal");
+        float moveVertical = Input.GetAxis ("Vertical");
+        Vector3 movement = new Vector3 (moveHorizontal, 0.0f, moveVertical);
+
+        if (Math.Abs(moveVertical) > 0 || Math.Abs(moveHorizontal) > 0.0)
+        {
+//            animator.Play("Robot Armature|Action Walk Done", -1, 0f);
+            animator.CrossFade("Walk", 0.0f, -1);
+        }
+        else
+        {
+            animator.CrossFade("Stand", 0.0f, -1);
+        }
+        
+        transform.position = transform.position + movement * speed;
+//        robot.AddForce (movement * speed);
+    }
+}
