6f74d879e218fb911eb3946f92118372ea727686
Montvydas Klumbys <montvydas.klumbys@gmail.com>
2018-03-28 00:57:54 +0100

    Added a walking mechanism with slights ups and downs, enabled jumping

--- Assets/Scripts/RobotController.cs
+++ Assets/Scripts/RobotController.cs
@@ -21,7 +21,30 @@ public class RobotController : MonoBehaviour
     // Update is called once per frame
     void Update () 
     {
-		
+//        if (Input.GetKey(KeyCode.UpArrow))
+//        {
+//            //Move the Rigidbody forwards constantly at speed you define (the blue arrow axis in Scene view)
+//            transform.velocity = transform.forward * speed;
+//            transform.position = transform.position + movement * speed;
+//        }
+//
+//        if (Input.GetKey(KeyCode.DownArrow))
+//        {
+//            //Move the Rigidbody backwards constantly at the speed you define (the blue arrow axis in Scene view)
+//            m_Rigidbody.velocity = -transform.forward * speed;
+//        }
+
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            //Rotate the sprite about the Y axis in the positive direction
+            transform.Rotate(new Vector3(0, 1, 0) * Time.deltaTime * speed * 5, Space.World);
+        }
+
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            //Rotate the sprite about the Y axis in the negative direction
+            transform.Rotate(new Vector3(0, -1, 0) * Time.deltaTime * speed * 5, Space.World);
+        }
     }
 
     // All physics go there
--- Assets/Scripts/WalkController.cs
+++ Assets/Scripts/WalkController.cs
@@ -1,57 +1,142 @@
-﻿using System.Collections;
+﻿using System;
 using System.Collections.Generic;
-using UnityEditor;
 using UnityEngine;
 
 public class WalkController : MonoBehaviour {
 	private Vector3 cameraSpeed;
 	private float allGravity = 0.0f;
 	private float distance = 0.0f;
+	private float jump = 0.0f;
 	private bool foundPoint = false;
 	private float prevAllGravity = 0.0f;
 	private float previousSystemTime = 0.0f;
 	private int stepNumber = 0;
 	float freq = 0.0f;
 	public float thrust = 2.0f;
+
+	private Rigidbody rigidbody; 
 	
+	[SerializeField] private float m_RenderScale = 2f;
 	// Use this for initialization
-	void Start () {
+	void Start ()
+	{
+//		UnityEngine.XR.XRSettings.renderViewportScale = m_RenderScale;
 		Input.gyro.enabled = true;
+		rigidbody = GetComponent<Rigidbody>();
+		
+		data = new List<double>();
+		count = 0;
 	}
+
+	private List<double> data;
+	private int count;
 	
 	// Update is called once per frame
-	private float gravityFront;
 	void Update () 
 	{
 		//transform.position += Camera.main.transform.forward * thrust * Time.deltaTime;
-		cameraSpeed = Camera.main.transform.forward * thrust * Time.deltaTime;	//get camera looking position as a Vector3 * thrust speed
+//		cameraSpeed = Camera.main.transform.forward * thrust * Time.deltaTime;	//get camera looking position as a Vector3 * thrust speed
 
-		allGravity = Input.acceleration.sqrMagnitude - 1;
+//		CharacterController controller = GetComponent<CharacterController>();
 		
-		float acc1 = Input.gyro.userAcceleration.sqrMagnitude;
-//		gravityFront = Input.gyro.userAcceleration.z;
-		float acc2 = Input.acceleration.sqrMagnitude;
-		float acc3 = Input.gyro.gravity.y;
+		float acc1 = Input.gyro.userAcceleration.y;
 
-//		accelAnalysis();
-		Debug.Log (string.Format ("userAcc, acc, gravity: {0:0.00} {1:0.00} {2:0.00}", acc1, acc2, acc3)); 
+		Vector3 gravityVector = Input.gyro.gravity;
+		Vector3 accVector = Input.gyro.userAcceleration;		
+		float projection = Vector3.Project(accVector, gravityVector).sqrMagnitude;
+		projection = Vector3.Dot(accVector, gravityVector) < 0 ? -projection : projection;
+
+//		Debug.Log (string.Format ("Projection, AccY: {0:0.000} {1:0.000}", projection, acc1));
+
+//		if (count > 150)
+//		{
+//			count = 0;
+//			Debug.LogError("Samples Taken");
+//
+//			string printValues = "[";
+//			data.ForEach(d =>
+//			{
+//				printValues += string.Format("{0:0.000}, ", d);
+//			});
+//			
+//			Debug.LogError(printValues);			
+//			data.Clear();
+//		}
+//		
+//		count++;
+//		data.Add(projection);
+		
+		
+		allGravity = projection;
+		AccelAnalysis();
 	}
 	
 	//add physics in here
 	void FixedUpdate ()
 	{
-		Vector3 velocity = cameraSpeed * distance;
+//		transform.position += transform.forward * thrust * Time.deltaTime * distance;
+//		rigidbody.MovePosition(transform.position + Camera.main.transform.forward * thrust * Time.deltaTime * distance);
+
+//		Vector3 movement = transform.position + Camera.main.transform.forward * thrust * Time.deltaTime * distance;
+		
+		Vector3 movement = transform.position + Camera.main.transform.forward * thrust * Time.deltaTime * distance;
+//		movement.y = 3 + distance / 5;
+//		rigidbody.velocity = movement;
+		
+//		RaycastHit hit;
+//		if (Physics.Raycast(transform.position, -transform.up, out hit, 3))
+//		{
+//			rigidbody.AddForce(transform.up*(10.0f/(hit.distance/2)));
+//		}
+		
+//		rigidbody.MovePosition(movement);
+
+
+		
+		Vector3 force = Camera.main.transform.forward * thrust * Time.deltaTime * distance;
+		
+//		rigidbody.MovePosition(movement);
+			
+//		rigidbody.velocity = movement;
+		
+		if (Input.touchCount > 0 || Input.GetMouseButton(0))
+		{
+			force = Camera.main.transform.forward * thrust * Time.deltaTime;
+		}
+		
+		force.y = thrust * Time.deltaTime * jump;
+		
+		rigidbody.AddForce(force);
+		
+		RaycastHit hit;
+		Ray downRay = new Ray(transform.position, -Vector3.up);
+		if (Physics.Raycast(downRay, out hit)) {
+			float hoverError = hoverHeight - hit.distance;
+			if (hoverError > 0) {
+				float upwardSpeed = rigidbody.velocity.y;
+				float lift = hoverError * hoverForce - upwardSpeed * hoverDamp;
+				rigidbody.AddForce(lift * Vector3.up);
+			}
+		}
+		
+//		transform.position += Vector3.forward * thrust * Time.deltaTime * distance;
+//		Camera.main.transform.forward;
+//		Vector3 velocity = cameraSpeed * distance;
+//		transform.position += transform.position + velocity;
 	}
 
+	public float hoverHeight = 3.0F;
+	public float hoverForce = 5.0F;
+	public float hoverDamp = 0.5F;
 	
 	void OnApplicationQuit() {
 		Input.gyro.enabled = false;
 	}
 	
-	private void accelAnalysis ()
+	private void AccelAnalysis ()
 	{
 //		float threshold = 0.07f;
-		float threshold = 0.5f;
+		float threshold = 0.05f;
 		bool isRising;
 
 		//Check if the curve is rising or falling
@@ -61,27 +146,44 @@ public class WalkController : MonoBehaviour {
 			isRising = false;
 		
 		//finds the first point
-		if ( (allGravity > threshold) && isRising && !foundPoint && Mathf.Abs(gravityFront) < 0.1f){
+		if ( allGravity > threshold && isRising && !foundPoint ){
 			foundPoint = true;
-			distance += Mathf.Abs(allGravity - prevAllGravity) * freq;
-			previousSystemTime = Time.time*1000f;
+//			distance += Mathf.Abs(allGravity - prevAllGravity) * freq;
+//			distance = (allGravity)*10;
+			distance = 1;
+			jump = distance / 3;
+//			previousSystemTime = Time.time*1000f;
 		}
+		
+		if ( allGravity > 3*threshold && isRising ){
+			distance = 0.5f;
+			jump = 6;
+		}
+		
+		if ( allGravity > 3*threshold && !isRising ){
+			distance = 0.3f;
+			jump = 0;
+		} 
 			
-		if (Time.time*1000f - previousSystemTime > 500.0f) {
-			distance = 0.0f;
-			freq = 0.0f;
-		}
-		//find the second point
-		if ((allGravity < -threshold) && !isRising && foundPoint) {
+//		if (Time.time*1000f - previousSystemTime > 500.0f) {
+//			distance = 0.0f;
+//			freq = 0.0f;
+//		}
+		
+		// find the second point => this previously was set to -therhold,
+		// but might be useful to use zero as well 
+		if ( allGravity < threshold && !isRising && foundPoint ) {
 			foundPoint = false;
-			float dT = Time.time*1000.0f - previousSystemTime;
-			freq = 1000.0f / dT;
+//			float dT = Time.time*1000.0f - previousSystemTime;
+//			freq = 1000.0f / dT;
 			//checks for noise
 //			
 //			if (freq < 15.0) {
 			stepNumber++;
+			Debug.LogError (string.Format ("Steps: {0}", stepNumber ));
 //				distance = freq / 2.0;
-			distance = 0.0f;
+			distance = 0;
+			jump = 0;
 //			}
 
 		}
